ef9f593c4ab39258c83509c02160f7fb44da3619
Pavan Sai <pavansaivuppala@gmail.com>
2024-03-31 19:52:57 -0400

    LoginScreenUIUpdated

--- app/src/main/java/abhishek/pathak/mockwithai/ui/theme/Color.kt
+++ app/src/main/java/abhishek/pathak/mockwithai/ui/theme/Color.kt
@@ -3,8 +3,11 @@ package abhishek.pathak.mockwithai.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
+val Green = Color(0xFF57A56B)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
