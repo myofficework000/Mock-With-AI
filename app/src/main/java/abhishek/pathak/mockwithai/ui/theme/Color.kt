127730936ef7167b4d54c512cc3804c4d46df13a
sravyaannadi1 <sravyaa2000@gmail.com>
2024-04-01 01:09:49 -0400

    Welcome page and interview schedule page added

--- app/src/main/java/abhishek/pathak/mockwithai/ui/theme/Color.kt
+++ app/src/main/java/abhishek/pathak/mockwithai/ui/theme/Color.kt
@@ -11,5 +11,5 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val White= Color(0xFFFFFFFF)
-val Green80= Color(0xFF478E54)
+val Green80= Color(0xFFB3D68A)
 val Green40= Color(0xFFCDF2D4)
