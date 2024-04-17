457853d7b4f331cb935cf9239a06832b03640608
Pavan Sai <pavansaivuppala@gmail.com>
2024-04-17 11:25:15 -0400

    Implemented Logout Button and logic

--- app/src/main/java/abhishek/pathak/mockwithai/view/screens/UserAccountScreen.kt
+++ app/src/main/java/abhishek/pathak/mockwithai/view/screens/UserAccountScreen.kt
@@ -1,6 +1,7 @@
 package abhishek.pathak.mockwithai.view.screens
 
 import abhishek.pathak.mockwithai.R
+import abhishek.pathak.mockwithai.navigation.NavigationItem
 import abhishek.pathak.mockwithai.ui.theme.dp_16
 import abhishek.pathak.mockwithai.ui.theme.dp_2
 import abhishek.pathak.mockwithai.ui.theme.dp_4
@@ -22,6 +23,7 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -31,19 +33,28 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
-fun UserAccountScreen() {
+fun UserAccountScreen(navController: NavController) {
     Scaffold { paddingValues ->
         Box(modifier = Modifier.padding(paddingValues)) {
             Column(modifier = Modifier.fillMaxSize()) {
                 ProfileHeader()
                 MenuBar()
                 ImageGallery()
+                Button(onClick = {navController.navigate(NavigationItem.LOGIN_SCREEN.route)}, modifier = Modifier.align(Alignment.CenterHorizontally)) {
+                    Text(
+                        text = "Logout",
+                        fontSize = sp_14,
+                    )
+                }
             }
         }
     }
@@ -76,6 +87,7 @@ fun ProfileHeader() {
             fontSize = sp_14,
             color = Color.Gray
         )
+
     }
 }
 
@@ -136,5 +148,5 @@ fun ImageItem(painter: Painter) {
 @Preview
 @Composable
 private fun UserAccountScreenPrev() {
-    UserAccountScreen()
+    UserAccountScreen(rememberNavController())
 }
